af67a4e41344e5a5f93518f16b51783a02d95c05
aldo <31267265+aldoavila26@users.noreply.github.com>
2017-09-27 15:36:31 -0500

    Update Biblioteca.php

--- Biblioteca.php
+++ Biblioteca.php
@@ -6,4 +6,22 @@ $comb=factorial($m)/(factorial($m-$s)*factorial($s));
 return $comb;
 }
 
+function factorial($m)
+{
+$fact=1;
+for ($i=1;$i<=$m;$i++){
+$fact=$fact*$i;}
+return $fact;
+}
+
+function binom($m,$p,$r)
+{
+$binom=combinatoria($m,$r)*pow($p,$r)*pow((1-$p),($m-$r));
+return $binom;
+	}
+
+
+
+
+
 ?>
