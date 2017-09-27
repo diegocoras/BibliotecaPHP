2b564a97886b9e479b34475d261bc398c5c1a37a
diegocoras <31228505+diegocoras@users.noreply.github.com>
2017-09-27 15:58:25 -0500

    Update Biblioteca.php

--- Biblioteca.php
+++ Biblioteca.php
@@ -6,4 +6,21 @@ $comb=factorial($m)/(factorial($m-$s)*factorial($s));
 return $comb;
 }
 
+function binomac($m,$p,$x)
+{
+$proba=0;
+for ($i=0;$i<=$x;$i++)
+{
+$proba=$proba+binom($m,$p,$i);}
+return $proba;
+}
+
+function promedio($lista) { 
+$sum=0;
+    for ($i=0;$i<count($lista);$i++) { 
+       $sum=$sum+$lista[$i] ; 
+    } 
+$prom=$sum/count($lista);
+return $prom;
+}
 ?>
